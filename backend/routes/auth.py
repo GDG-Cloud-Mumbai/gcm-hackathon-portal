3d591ecf91f0f26051bdaa40bda894256b3ab23c
Manan Gandhi <ardumanan@gmail.com>
2026-06-03 22:10:33 +0530

    POST -> PATCH

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -7,11 +7,11 @@ from handlers.auth import (
     verify_otp,
     update_profile,
 )
-from models.user import UserPublic
+from models.user import UserPrivate
 
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 router.post("/request-otp")(request_otp)
 router.post("/verify-otp", response_model=AuthTokenResponse)(verify_otp)
-router.get("/me", response_model=UserPublic)(auth_me)
-router.post("/update-profile", response_model=UserPublic)(update_profile)
+router.get("/me", response_model=UserPrivate)(auth_me)
+router.patch("/update-profile", response_model=UserPrivate)(update_profile)
