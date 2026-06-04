3406c34f1915ff40d55ee0e19556dcf181afd8a3
Manan Gandhi <ardumanan@gmail.com>
2026-06-04 23:55:48 +0530

    User model changes

--- backend/models/user.py
+++ backend/models/user.py
@@ -5,9 +5,16 @@ from pydantic import BaseModel, EmailStr
 class Role(BaseModel):
     name: str
 
+    def __init__(self, **data):
+        super().__init__(**data)
+        if self.name not in {"user", "admin", "superadmin"}:
+            raise ValueError("Invalid role name")
 
-class UserPublic(BaseModel):
+
+class UserPrivate(BaseModel):
+    uuid: str
     email: str
+    username: str | None = None
     name: str
     global_role: Role = Role(name="user")
     created_at: str | datetime | None = None
@@ -21,9 +28,17 @@ class UserPublic(BaseModel):
             self.last_login_at = datetime.fromisoformat(self.last_login_at)
 
 
+class UserPublic(BaseModel):
+    uuid: str
+    username: str | None = None
+    name: str
+
+
 class User(BaseModel):
     _id: str | None = None
+    uuid: str
     email: EmailStr
+    username: str | None = None
     name: str
     password: str | None = None
     global_role: Role = Role(name="user")
@@ -37,9 +52,10 @@ class User(BaseModel):
         if isinstance(self.last_login_at, str):
             self.last_login_at = datetime.fromisoformat(self.last_login_at)
 
-    def to_public(self) -> UserPublic:
-        return UserPublic(
+    def to_public(self) -> UserPrivate:
+        return UserPrivate(
             email=self.email,
+            username=self.username,
             name=self.name,
             global_role=self.global_role,
             created_at=self.created_at,
